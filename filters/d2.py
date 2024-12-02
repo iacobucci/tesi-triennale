14ed01140149b786fbed1b61cc7f0d0fdea267c9
iacobucci <iacobuccivalerio@gmail.com>
2024-12-03 00:35:15 +0100

    new file:   d2/out.pdf 	new file:   d2/out.svg 	modified:   d2/rendering_modes.d2 	modified:   filters/d2.py

--- filters/d2.py
+++ filters/d2.py
@@ -17,27 +17,21 @@ d2-config: {
 
 """ + elem.text
 
-		with open("log.txt", "w") as f:
-			f.write(code_content)
+		with tempfile.NamedTemporaryFile(suffix=".d2", delete=False) as d2, tempfile.NamedTemporaryFile(suffix=".svg", delete=False) as svg, tempfile.NamedTemporaryFile(suffix=".pdf", delete=False) as pdf:
+			d2.write(code_content.encode('utf-8'))
 
-		with tempfile.NamedTemporaryFile(suffix=".d2", delete=False) as input_file, tempfile.NamedTemporaryFile(suffix=".png", delete=False) as output_file:
-			input_path = input_file.name
-			output_path = output_file.name
-			# Scrivere il contenuto del blocco nel file temporaneo
-			input_file.write(code_content.encode('utf-8'))
-		
 		try:
 			# Eseguire il comando d2
-			subprocess.run(["d2", input_path, output_path], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+			subprocess.run(["d2", d2.name, svg.name], check=True) #, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+			subprocess.run(["inkscape", svg.name, "--export-filename=" + pdf.name], check=True) #, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 			# Restituire un blocco LaTeX che include il file PDF generato
-			return pf.RawBlock("\\begin{center}\\includegraphics[height=" + elem.attributes["height"] +",keepaspectratio]{ " + output_path + "}\\end{center}", format="latex")
+			return pf.RawBlock("\\begin{center}\\includegraphics[height=" + elem.attributes["height"] +",keepaspectratio]{ " + pdf.name + "}\\end{center}", format="latex")
 		except subprocess.CalledProcessError as e:
 			pf.debug(f"Errore durante l'esecuzione di d2: {e}")
 			return elem  # Lascia il blocco intatto in caso di errore
 		finally:
-			# Pulire il file di input (il PDF potrebbe servire ancora)
-			os.remove(input_path)
+			os.remove(d2.name)
 
 def main(doc=None):
 	return pf.run_filter(intercept_codeblock, doc=doc)
